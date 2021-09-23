2b478db85090d0932f8eec1b0a00a3e038e6e10c
Jurek Elliesen <jurekelliesen@web.de>
2021-09-23 13:06:42 +0200

    minor change

--- snakeai/base.py
+++ snakeai/base.py
@@ -14,8 +14,7 @@ class AgentBase:
 
     def save(self, root, agent_name):
         write_to_file(self, root / f"{agent_name}.pkl")
-        write_to_file(dict_to_string(self.params), root / f"{agent_name}.yml",
-                      text=True)
+        write_to_file(dict_to_string(self.params), root / f"{agent_name}.yml", text=True)
 
     def __getattr__(self, name):
         if name in self.__dict__['params']:
@@ -24,7 +23,7 @@ class AgentBase:
             raise AttributeError
 
     def __setattr__(self, name, value):
-        if name != 'params' and name in self.params:
+        if name != 'params' and name in self.__dict__.get('params', []):
             self.params[name] = value
         else:
             self.__dict__[name] = value
