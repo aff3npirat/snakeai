527bd713e5689531a3a7e28f8728c1226aaa3df3
Jurek Elliesen <jurekelliesen@web.de>
2021-09-28 16:54:44 +0200

    removed self.params from view call
    forgot to remove in earlier commit.

--- snakeai/agents/qnet.py
+++ snakeai/agents/qnet.py
@@ -41,7 +41,7 @@ class QNetLearning:
         return random.choices([0, 1, 2, 3], weights=action_probs)
 
     def get_state(self, game):
-        return self.view(game, self.params)
+        return self.view(game)
 
     def train_episode(self, game, render):
         game.reset()
