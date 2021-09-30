860a5995f4fcb5b543fdb22f05d3093c0c4fb9a9
aff3npirat <jurek.elliesen@web.de>
2021-09-30 19:55:27 +0200

    Implement QNetSarsa

--- snakeai/agents/qnet.py
+++ snakeai/agents/qnet.py
@@ -7,7 +7,7 @@ from torch.nn import functional
 from snakeai.base import QAgentBase
 
 
-class QNet(nn.Module):
+class LinNet(nn.Module):
 
     def __init__(self, in_size, hidden_size, out_size):
         super().__init__()
@@ -26,12 +26,11 @@ class QNet(nn.Module):
         return self(item).tolist()
 
 
-# TODO: QNetSarsa
-class QNetLearning(QAgentBase):
+class QNetQLearning(QAgentBase):
 
     def __init__(self, params, name, vision, eps_greedy):
         super().__init__(params, name, vision, eps_greedy)
-        self.Q = QNet(params['in_size'], params['hidden_size'], 4)
+        self.Q = LinNet(params['in_size'], params['hidden_size'], 4)
         self.memory = deque(maxlen=100_000)
         self.optimizer = optim.Adam(self.Q.parameters(), lr=params['lr'])
         self.criterion = nn.MSELoss()
@@ -44,8 +43,8 @@ class QNetLearning(QAgentBase):
             action = self.get_action(state)
             done, reward = game.play_step(action)
             next_state = self.get_state(game)
-            self._train(state, action, reward, next_state, done)
             self.memory.append((state, action, reward, next_state, done))
+            self._train(state, action, reward, next_state, done)
             state = next_state
         self._train_long_memory(1000)
         self.params['n_games'] += 1
@@ -55,7 +54,6 @@ class QNetLearning(QAgentBase):
             mini_sample = random.sample(self.memory, batch_size)
         else:
             mini_sample = self.memory
-
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self._train(states, actions, rewards, next_states, dones)
 
@@ -80,6 +78,69 @@ class QNetLearning(QAgentBase):
             else:
                 gamma = self.params['gamma']
                 target[i][actions[i].item()] = rewards[i] + gamma * max(self.Q(next_states[i]))
+
+        self.Q.zero_grad()  # clear gradient buffers
+        loss = self.criterion(pred, target)
+        loss.backward()
+        self.optimizer.step()
+
+
+class QNetSarsa(QAgentBase):
+
+    def __init__(self, params, name, vision, eps_greedy):
+        super().__init__(params, name, vision, eps_greedy)
+        self.Q = LinNet(params['in_size'], params['hidden_size'], 4)
+        self.memory = deque(maxlen=100_000)
+        self.optimizer = optim.Adam(self.Q.parameters(), lr=params['lr'])
+        self.criterion = nn.MSELoss()
+
+    def train_episode(self, game):
+        done = False
+        state = self.get_state(game)
+        action = self.get_action(state)
+        while not done:
+            done, reward = game.play_step(action)
+            next_state = self.get_state(game)
+            next_action = self.get_action(next_state)
+            self.memory.append((state, action, reward, next_state, next_action, done))
+            self._train(state, action, reward, next_state, next_action, done)
+            state = next_state
+            action = next_action
+        self._train_long_memory(1000)
+        self.params['n_games'] += 1
+
+    def _train_long_memory(self, batch_size):
+        if len(self.memory) > batch_size:
+            mini_sample = random.sample(self.memory, batch_size)
+        else:
+            mini_sample = self.memory
+        self._train(*zip(*mini_sample))
+
+    def _train(self, states, actions, rewards, next_states, next_actions, dones):
+        states = torch.tensor(states, dtype=torch.float)
+        actions = torch.tensor(actions, dtype=torch.int)
+        rewards = torch.tensor(rewards, dtype=torch.float)
+        next_states = torch.tensor(next_states, dtype=torch.float)
+        next_actions = torch.tensor(next_actions, dtype=torch.int)
+
+        if len(states.shape) == 1:
+            states = torch.unsqueeze(states, 0)
+            actions = torch.unsqueeze(actions, 0)
+            rewards = torch.unsqueeze(rewards, 0)
+            next_states = torch.unsqueeze(next_states, 0)
+            next_actions = torch.unsqueeze(next_actions, 0)
+            dones = (dones,)
+
+        pred = self.Q(states)
+        target = pred.clone()
+        for i in range(len(dones)):
+            if dones[i]:
+                target[i][actions[i].item()] = rewards[i]
+            else:
+                gamma = self.params['gamma']
+                action = actions[i].item()
+                next_action = next_actions[i].item()
+                target[i][action] = rewards[i] + gamma * self.Q(next_states[i])[next_action]
 
         self.Q.zero_grad()  # clear gradient buffers
         loss = self.criterion(pred, target)
