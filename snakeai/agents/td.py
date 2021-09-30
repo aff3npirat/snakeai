a387a5117fae1079f777797bcf849f613bd0cc1e
aff3npirat <jurek.elliesen@web.de>
2021-09-30 19:55:14 +0200

    Implement TDQLearning

--- snakeai/agents/td.py
+++ snakeai/agents/td.py
@@ -1,7 +1,6 @@
 from snakeai.base import QAgentBase
 
 
-# TODO: TDQLearning
 class TDSarsa(QAgentBase):
 
     def __init__(self, params, name, vision, eps_greedy):
@@ -16,9 +15,30 @@ class TDSarsa(QAgentBase):
             done, reward = game.play_step(action)
             next_state = self.get_state(game)
             next_action = self.get_action(next_state)
-            target = reward + self.params['gamma'] * self.Q[next_state][next_action]
+            target = reward + self.params['gamma']*self.Q[next_state][next_action]
             delta = target - self.Q[state][action]
             self.Q[state][action] += self.params['lr'] * delta
             state = next_state
             action = next_action
+        self.params['n_games'] += 1
+
+
+# TODO: move game.reset() to train()
+class TDQLearning(QAgentBase):
+
+    def __init__(self, params, name, vision, eps_greedy):
+        super().__init__(params, name, vision, eps_greedy)
+
+    def train_episode(self, game):
+        game.reset()
+        done = False
+        state = self.get_state(game)
+        action = self.get_action(state)
+        while not done:
+            done, reward = game.play_step(action)
+            next_state = self.get_state(game)
+            target = reward + self.params['gamma']*max(self.Q[next_state])
+            delta = target - self.Q[state][action]
+            self.Q[state][action] += self.params['lr'] * delta
+            state = next_state
         self.params['n_games'] += 1
