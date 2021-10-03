38c6468737896048d1d049b902c4d2c43011eeb1
aff3npirat <jurek.elliesen@web.de>
2021-10-03 14:45:46 +0200

    Add evaluate_architectures
    is implemented poorly, should be reworked for better readability.

--- snakeai/nn_architectures.py
+++ snakeai/nn_architectures.py
@@ -4,11 +4,9 @@ from torch.nn import functional
 
 class NLinearNet(nn.Module):
 
-    def __init__(self, n, units, use_sigmoid=False):
+    def __init__(self, units, use_sigmoid=False):
         super().__init__()
-        if n + 1 != len(units):
-            raise ValueError(f"expected {n} layers, got {len(units) - 1}")
-        self.layers = [nn.Linear(units[i - 1], units[i]) for i in range(1, n)]
+        self.layers = [nn.Linear(units[i - 1], units[i]) for i in range(1, len(units))]
         self.architecture = str(units)
         self.use_sigmoid = use_sigmoid
 
