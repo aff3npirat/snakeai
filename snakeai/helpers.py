1bbaf7c3ea6a7f77d87466c61fd5750c31497c8a
aff3npirat <jurek.elliesen@web.de>
2021-10-05 18:30:04 +0200

    Add print_progress_bar

--- snakeai/helpers.py
+++ snakeai/helpers.py
@@ -66,3 +66,24 @@ def read_from_file(fpath, text=False):
                 return data
     except FileNotFoundError:
         return None
+
+
+def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='#'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + '-' * (length - filled_length)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end="")
+    # Print New Line on Complete
+    if iteration == total:
+        print()
