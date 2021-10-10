dca8837e69ca1934219ad7f495aa8d7916f7d279
Jurek <jurek.elliesen@web.de>
2021-10-10 11:18:48 +0200

    Add evaluate_td()

--- snakeai/main.py
+++ snakeai/main.py
@@ -5,18 +5,26 @@ from collections import defaultdict
 from datetime import datetime
 
 from snakeai import root_dir
-from snakeai.agents import EveryVisit, FirstVisit
-from snakeai.model import partial_vision, diagonal_vision, full_vision, short_vision
+from snakeai.agents import EveryVisit, FirstVisit, TDSarsa, TDQLearning
+from snakeai.model import partial_vision, diagonal_vision, full_vision, \
+    short_vision
 from snakeai.model import simple_eps_decay, lin_eps_decay, constant_eps
 from snakeai.helpers import plot, read_from_file, save_plot, print_progress_bar
 from snakeai.snake_game import SnakeGame
 
 
-def train_agent(agent=None, agent_file=None, h=20, w=20, episodes=1, save=True, verbose=1):
+def train_agent(agent=None,
+                agent_file=None,
+                h=20,
+                w=20,
+                episodes=1,
+                save=True,
+                verbose=1
+                ):
     if agent_file is not None and (root_dir / "agents" / agent_file).is_file():
         agent = read_from_file(root_dir / "agents" / agent_file)
         print(f"Loaded agent {agent.name}")
-    game = SnakeGame(w, h, verbose>=3)
+    game = SnakeGame(w, h, verbose >= 3)
 
     plot_scores = []
     plot_mean_scores = []
@@ -99,6 +107,65 @@ def evaluate_monte_carlo():
                                    suffix="complete",
                                    length=80)
     return {"ev": dict(data_ev), "fv": dict(data_fv)}
+
+
+def evaluate_td():
+    lrs = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9,
+           1.0]
+    epss = np.linspace(0.1, 1.0, 10)
+    gammas = np.linspace(0.0, 1.0, 11)
+    ms = np.linspace(0.1, 2, 20)
+    visions = {full_vision: "full",
+               partial_vision: "partial",
+               diagonal_vision: "diagonal",
+               short_vision: "short"}
+    sarsa = defaultdict(lambda: {})
+    qlearning = defaultdict(lambda: {})
+    i = 0
+    print_progress_bar(i,
+                       5720,
+                       prefix="Progress:",
+                       suffix="complete",
+                       length=80)
+    for lr in lrs:
+        for eps in epss:
+            for gamma in gammas:
+                for vision in visions:
+                    p = {"eps": eps, "gamma": gamma, "lr": lr}
+                    # simple
+                    model_id = f"{visions[vision]}+simple"
+                    param_id = str([eps, gamma, lr])
+                    sarsa_agent = TDSarsa(p, "", vision, simple_eps_decay)
+                    qlearn_agent = TDQLearning(p, "", vision, simple_eps_decay)
+                    train(sarsa_agent)
+                    train(qlearn_agent)
+                    sarsa[model_id][param_id] = evaluate(sarsa_agent)
+                    qlearning[model_id][param_id] = evaluate(qlearn_agent)
+                    # constant
+                    model_id = f"{visions[vision]}+const"
+                    sarsa_agent = TDSarsa(p, "", vision, constant_eps)
+                    qlearn_agent = TDQLearning(p, "", vision, constant_eps)
+                    train(sarsa_agent)
+                    train(qlearn_agent)
+                    sarsa[model_id][param_id] = evaluate(sarsa_agent)
+                    qlearning[model_id][param_id] = evaluate(qlearn_agent)
+                    # linear
+                    model_id = f"{visions[vision]}+lin"
+                    for m in ms:
+                        param_id = str([eps, gamma, lr, m])
+                        sarsa_agent = TDSarsa(p, "", vision, lin_eps_decay)
+                        qlearn_agent = TDQLearning(p, "", vision, lin_eps_decay)
+                        train(sarsa_agent)
+                        train(qlearn_agent)
+                        sarsa[model_id][param_id] = evaluate(sarsa_agent)
+                        qlearning[model_id][param_id] = evaluate(qlearn_agent)
+                    i += 1
+                    print_progress_bar(i,
+                                       5720,
+                                       prefix="Progress:",
+                                       suffix="complete",
+                                       length=80)
+    return {"sarsa": dict(sarsa), "qlearning": dict(qlearning)}
 
 
 def train(agent):
