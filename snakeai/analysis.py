7f3fbc36a35f6722621380b137bcb28a32ef56ef
Jurek <jurek.elliesen@web.de>
2021-10-13 16:38:44 +0200

    Add max and mean tables

--- snakeai/analysis.py
+++ snakeai/analysis.py
@@ -3,21 +3,27 @@ import pandas as pd
 from . import helpers
 
 
-def small_table(data):
+def small_table(data, peak=False):
     """
     Parameters
     ----------
     data : dict
         Containing evaluation data for single agent.
+    peak : bool, optional
+        If true table cells contain maximum score, if false cells contain mean
+        score.
     """
-    dfs = tables(data)
+    if peak:
+        dfs = max_tables(data)
+    else:
+        dfs = mean_tables(data)
     max_df = []
     for decay_id in ["simple", "lin", "const"]:
         max_df.append(dfs[decay_id].max(0))
     return pd.DataFrame(max_df, index=["simple", "lin", "const"])
 
 
-def tables(data):
+def mean_tables(data):
     """
     Parameters
     ----------
@@ -26,7 +32,7 @@ def tables(data):
 
     Returns
     -------
-    A dictionary containing a Dataframe for each eps-decay.
+    A dictionary containing a DataFrame for each eps-decay.
     """
     # data[vision][decay][params]
     dfs = {}
@@ -43,6 +49,31 @@ def tables(data):
                 row_labels.append(f"[{eps:.3}, {gamma:.3}, {m:.3}]")
 
         rows = [[f"{sum(scores) / len(scores):.2f}"
+                 for vision in col_labels
+                 if (scores := data[vision][decay_id][param]) is not None]
+                for param in data["full"][decay_id]]
+        dfs[decay_id] = pd.DataFrame(rows,
+                                     index=row_labels,
+                                     columns=col_labels,
+                                     dtype=float)
+    return dfs
+
+
+def max_tables(data):
+    dfs = {}
+    col_labels = ["full", "partial", "diagonal", "short"]
+    for decay_id in ["simple", "const", "lin"]:
+        row_labels = []
+        for param_id in data["full"][decay_id]:
+            params = param_id[1:-1].split(", ")
+            if len(params) == 2:
+                eps, gamma = params
+                row_labels.append(f"[{eps:.3}, {gamma:.3}]")
+            else:
+                eps, gamma, m = params
+                row_labels.append(f"[{eps:.3}, {gamma:.3}, {m:.3}]")
+
+        rows = [[f"{max(scores):.2f}"
                  for vision in col_labels
                  if (scores := data[vision][decay_id][param]) is not None]
                 for param in data["full"][decay_id]]
@@ -74,7 +105,8 @@ def html_tables(data, root_dir):
     """Save data as html tables.
     
     Creates four files (simple.html, const.html, lin.html, max.html) in
-    specified root directory.
+    specified root directory. Each file contains a table representing the peak
+    and mean performances.
     
     Parameters
     ----------
@@ -83,8 +115,13 @@ def html_tables(data, root_dir):
     root_dir : str
         Relative path to directory where tables should be saved to.
     """
-    dfs = tables(data)
-    dfs["max"] = small_table(data)
-    for key in dfs:
-        html_table = dfs[key].to_html()
+    mean_dfs = mean_tables(data)
+    max_dfs = max_tables(data)
+    mean_dfs["max"] = small_table(data, peak=False)
+    max_dfs["max"] = small_table(data, peak=True)
+    for key in mean_dfs:
+        df = pd.concat([mean_dfs[key], max_dfs[key]],
+                       axis=1,
+                       keys=["mean", "max"])
+        html_table = df.to_html()
         helpers.write_to_file(html_table, f"{root_dir}/{key}.html", text=True)
